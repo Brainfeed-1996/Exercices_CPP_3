dd0e8721d1d619da9bd588a4e0de5eec47effd60
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-05-10 21:36:52 +0200

    Update Exercice3.cpp

--- Exercice3.cpp
+++ Exercice3.cpp
@@ -8,9 +8,9 @@ main()
     float x = 1.25;
     double z = 5.5;
 
-    cout << n + c + p << endl; //1010
-    cout << 2 * x + c << endl; //7.5
-    cout << (char)n + c << endl; //10
-    cout << (float)z + n / 2 << endl; //7.5
+    cout << n + c + p << endl; //1010 long
+    cout << 2 * x + c << endl; //7.5 float
+    cout << (char)n + c << endl; //10 int
+    cout << (float)z + n / 2 << endl; //7.5 float
 
 }
